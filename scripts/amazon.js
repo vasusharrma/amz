547f5ea0a318437905d26e351120d5ed013ba57b
vasusharrma <sharmavashu099@gmail.com>
2025-06-10 21:08:08 +0530

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,44 +1,15 @@
+import products from "../data/products.js";
+
 const productGridElement = document.querySelector(".products-grid");
-
-const product = [
-  {
-    imageURL: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    imageURL: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-
-  {
-    imageURL: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
 
 function renderProducts() {
   let html = "";
-  product.forEach((e) => {
+  products.forEach((e) => {
     html += `
     
     <div class='product-container'>
    <div class='product-image-container'>
-    <img class='product-image' src = '${e.imageURL}'>
+    <img class='product-image' src = '${e.image}'>
    </div>
 
           <div class="product-name limit-text-to-2-lines">
